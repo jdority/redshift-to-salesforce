06786485d938c4b25c89fa7cc1d65ef79994f266
J Dority <jdority@salesforce.com>
2018-01-25 21:05:42 -0800

    Update public_category_after_insert.sql

--- public_category_after_insert.sql
+++ public_category_after_insert.sql
@@ -6,10 +6,9 @@
 # After AWS Glue writes a row to the public staging schema, this function will upsert the row into the salesforce schema
 # so Heroku Connect can then sync to salesforce;
 #
-# A constraint is required to use ON CONFLICT syntax.
-# CREATE UNIQUE INDEX CONCURRENTLY catid_pk_idx ON category (catid);
-# ALTER TABLE category 
-#    ADD CONSTRAINT category_pkey PRIMARY KEY USING INDEX catid_pk_idx;
+# A unique constraint is required to use ON CONFLICT syntax.   Heroku Connect creates them with hcu_ naming convention.
+# ALTER TABLE salesforce.category__c 
+#   ADD CONSTRAINT catid__c_unq UNIQUE USING INDEX hcu_idx_category__c_catid__c;
 #
 # public_category_after_insert()
 
@@ -18,18 +17,24 @@ CREATE OR REPLACE FUNCTION public_category_after_insert()
 
     $BODY$
         BEGIN
-            INSERT INTO salesforce.category__c VALUES (NEW.catid, NEW.catgroup, NEW.catname, NEW.catdesc)
-            ON CONFLICT (category_pkey )
-                DO UPDATE SET (catgroup__c, name, catdesc__c) = (NEW.catgroup, NEW.catname, NEW.catdesc) WHERE catid__c = NEW.catid;
-     END;
+            IF pg_trigger_depth() <> 1 THEN
+                RETURN NEW;
+            END IF;
+            INSERT INTO salesforce.category__c(id, catid__c, catgroup__c, name, catdesc__c)
+                 VALUES (NEXTVAL('salesforce.category__c_id_seq'::regclass),NEW.catid, NEW.catgroup, NEW.catname, NEW.catdesc)
+                 ON CONFLICT ON CONSTRAINT catid__c_unq
+                 DO UPDATE SET (catgroup__c, name, catdesc__c) = (NEW.catgroup, NEW.catname, NEW.catdesc);
+               RETURN NULL;
+         END; 
     $BODY$
     LANGUAGE plpgsql;
 
 # public_category_after_insert
 
-    CREATE TRIGGER public_category_after_insert
-       AFTER INSERT on public.category
-    FOR EACH ROW EXECUTE PROCEDURE public_category_after_insert();
-    
+CREATE TRIGGER public_category_after_insert
+ AFTER INSERT on public.category
+   FOR EACH ROW EXECUTE PROCEDURE public_category_after_insert();
+
+# Other handy syntax   
 # DROP FUNCTION public_category_after_insert();
 # DROP TRIGGER public_category_after_insert ON category;
