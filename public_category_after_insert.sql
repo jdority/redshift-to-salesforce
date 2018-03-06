ad30dca2a69863b1aa2b589cf84cb48619a8278c
J Dority <jdority@salesforce.com>
2018-03-06 09:38:30 -0800

    Update public_category_after_insert.sql

--- public_category_after_insert.sql
+++ public_category_after_insert.sql
@@ -1,17 +1,21 @@
-#
-# Heroku Postgres Schemas:
-#    * public - staging schema where AWS GLUE writes the Redshift rows for the category table.
-#    * salesforce - this is the schema that Heroku Connect uses.
-# 
-# After AWS Glue writes a row to the public staging schema, this function will upsert the row into the salesforce schema
-# so Heroku Connect can then sync to salesforce;
-#
-# A unique constraint is required to use ON CONFLICT syntax.   Heroku Connect creates them with hcu_ naming convention.
-# ALTER TABLE salesforce.category__c 
-#   ADD CONSTRAINT catid__c_unq UNIQUE USING INDEX hcu_idx_category__c_catid__c;
-#
-# public_category_after_insert()
+--
+--Heroku Postgres Schemas:
+--  * public - staging schema where AWS GLUE writes the Redshift rows for the category table.
+--  * salesforce - this is the schema that Heroku Connect uses.
 
+--After AWS Glue writes a row to the public staging schema, this function will upsert the row into the salesforce schema
+--so Heroku Connect can then sync to salesforce;
+
+
+--STEP 1:  A unique constraint is required to use ON CONFLICT syntax.  Heroku Connect creates with hcu_ naming convention.
+ALTER TABLE salesforce.category__c 
+   ADD CONSTRAINT catid__c_unq UNIQUE USING INDEX hcu_idx_category__c_catid__c;
+   
+
+--public_category_after_insert()
+--------------------------------------------------------------------------------------------------------------------------
+--STEP 2:  This function performs an UPSERT into the salesforce.category__c table.   AWS does not support an UPSERT 
+--------------------------------------------------------------------------------------------------------------------------
 CREATE OR REPLACE FUNCTION public_category_after_insert()
     RETURNS trigger AS
 
@@ -22,20 +26,28 @@ CREATE OR REPLACE FUNCTION public_category_after_insert()
             END IF;
             INSERT INTO salesforce.category__c(id, catid__c, catgroup__c, name, catdesc__c)
                  VALUES (NEXTVAL('salesforce.category__c_id_seq'::regclass),NEW.catid, NEW.catgroup, NEW.catname, NEW.catdesc)
-                 ON CONFLICT ON CONSTRAINT catid__c_unq
+                 ON CONFLICT ON CONSTRAINT catid__c_unq -- UPSERT
                  DO UPDATE SET (catgroup__c, name, catdesc__c) = (NEW.catgroup, NEW.catname, NEW.catdesc);
+                 ------------------------------------------------------------------------------------------------
+                 -- This statement will remove row from staging table public.category AFTER the UPSERT completes,
+                 -- again because AWS Glue cannot TRUNCATE a table, it can only re-create
                  DELETE from category where catid = NEW.catid;
+                 ------------------------------------------------------------------------------------------------
                RETURN NULL;
          END; 
     $BODY$
     LANGUAGE plpgsql;
 
-# public_category_after_insert
 
+---------------------------------------------------------------------------------------------------------------------------
+-- STEP 3: After AWS Glue writes a row of data into the staging table public.category - a procedure will be called to update 
+-- salesforce.category__c schema which is referenced in Heroku Connect
+---------------------------------------------------------------------------------------------------------------------------
+-- public_category_after_insert
 CREATE TRIGGER public_category_after_insert
  AFTER INSERT on public.category
    FOR EACH ROW EXECUTE PROCEDURE public_category_after_insert();
 
-# Other handy syntax   
-# DROP FUNCTION public_category_after_insert();
-# DROP TRIGGER public_category_after_insert ON category;
+-- Other handy syntax   
+-- DROP FUNCTION public_category_after_insert();
+-- DROP TRIGGER public_category_after_insert ON category;
